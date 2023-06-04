f06913893cd9a0c196c6f1b774692861092f0512
Félix Vicente <felixvicent1306@gmail.com>
2023-06-04 14:24:45 -0300

    create category and find by id

--- src/main/java/com/felps/algamoney/api/resource/CategoryResource.java
+++ src/main/java/com/felps/algamoney/api/resource/CategoryResource.java
@@ -1,14 +1,24 @@
 package com.felps.algamoney.api.resource;
 
+import java.net.URI;
 import java.util.List;
+import java.util.Optional;
 
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.http.HttpStatus;
+import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PathVariable;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
+import org.springframework.web.servlet.support.ServletUriComponentsBuilder;
 
 import com.felps.algamoney.api.model.Category;
 import com.felps.algamoney.api.repository.CategoryRepository;
+
+import jakarta.servlet.http.HttpServletResponse;
 
 @RestController
 @RequestMapping("/categories")
@@ -20,5 +30,23 @@ public class CategoryResource {
   @GetMapping
   public List<Category> list() {
     return categoryRepository.findAll();
+  }
+
+  @PostMapping
+  public ResponseEntity<Category> store(@RequestBody Category category, HttpServletResponse response) {
+    Category newCategory = categoryRepository.save(category);
+    
+    URI uri = ServletUriComponentsBuilder
+      .fromCurrentRequestUri()
+      .path("/{id}")
+      .buildAndExpand(newCategory.getId())
+      .toUri();
+
+    return ResponseEntity.created(uri).body(newCategory);
+  }
+
+  @GetMapping("/{id}")
+  public Optional<Category> getById(@PathVariable Long id){
+    return categoryRepository.findById(id);
   }
 }
