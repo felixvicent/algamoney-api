7af1eefe91982a15fbdc654ac774fbb3d4d4afc8
Félix Vicente <felixvicent1306@gmail.com>
2023-06-05 08:50:40 -0300

    adjust exceptions

--- src/main/java/com/felps/algamoney/api/exceptionhandler/AlgamoneyExceptionHandler.java
+++ src/main/java/com/felps/algamoney/api/exceptionhandler/AlgamoneyExceptionHandler.java
@@ -4,9 +4,11 @@ import java.util.ArrayList;
 import java.util.Arrays;
 import java.util.List;
 
+import org.apache.commons.lang3.exception.ExceptionUtils;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.context.MessageSource;
 import org.springframework.context.i18n.LocaleContextHolder;
+import org.springframework.dao.DataIntegrityViolationException;
 import org.springframework.dao.EmptyResultDataAccessException;
 import org.springframework.http.HttpHeaders;
 import org.springframework.http.HttpStatus;
@@ -49,6 +51,17 @@ public class AlgamoneyExceptionHandler extends ResponseEntityExceptionHandler {
 
     List<Error> errors = createErrorList(ex.getBindingResult());
     return handleExceptionInternal(ex, errors, headers, HttpStatus.BAD_REQUEST, request);
+  }
+
+  @ExceptionHandler({ DataIntegrityViolationException.class })
+  public ResponseEntity<Object> handleDataIntegrityViolationException(DataIntegrityViolationException ex,
+      WebRequest request) {
+    String userMessage = messageSource.getMessage("resource.bad-request", null, LocaleContextHolder.getLocale());
+    String devMessage = ExceptionUtils.getRootCauseMessage(ex);
+
+    List<Error> errors = Arrays.asList(new Error(userMessage, devMessage));
+
+    return handleExceptionInternal(ex, errors, new HttpHeaders(), HttpStatus.BAD_REQUEST, request);
   }
 
   private List<Error> createErrorList(BindingResult bindingResult) {
